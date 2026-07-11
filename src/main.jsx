0ae9f736908e9fcfe19165e0c235e629ae778149
Vibe <vibe@ionos.com>
2026-07-11 12:01:52 +0000

    Fix: Vorschau-Routing robuster gemacht mit app-base Meta-Tag Unterstuetzung
    MessageId: 376

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,11 @@ import { BrowserRouter } from "react-router";
 import App from "./App.jsx";
 import "./index.css";
 
-const basename = new URL(document.baseURI).pathname.replace(/\/$/, "");
+// Use app-base meta tag if present (preview), otherwise derive from document.baseURI
+const metaBase = document.querySelector('meta[name="app-base"]')?.getAttribute("content");
+const basename = metaBase
+  ? metaBase.replace(/\/$/, "")
+  : new URL(document.baseURI).pathname.replace(/\/$/, "");
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
